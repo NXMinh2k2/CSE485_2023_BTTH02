fbe99eb89ff13c3d5b4a6c3418ef1c7a0dfd74af
phongngo1152 <99382324+phongngo1152@users.noreply.github.com>
2023-03-19 10:15:46 +0700

    artile

--- CSE485_2023_BTTH02/models/Article.php
+++ CSE485_2023_BTTH02/models/Article.php
@@ -1,25 +1,20 @@
 <?php
-class Article {
-    private $hinhanh;
-    private $tieude;
-    private $ma_bviet;
+class Article{
+    // Thuộc tính
 
-    public function __construct($hinhanh, $tieude, $ma_bviet){
-        $this->hinhanh = $hinhanh;
-        $this->tieude = $tieude;
-        $this->ma_bviet = $ma_bviet;
+    private $title;
+    private $summary;
+    private $cat_name;
+
+
+    public function __construct($title, $summary,$cat_name){
+        $this->title = $title;
+        $this->summary = $summary;
+        $this->cat_name = $cat_name;
     }
 
     // Setter và Getter
-    public function getHinhAnh(){
-        return $this->hinhanh;
-    }
-
-    public function getTieuDe(){
-        return $this->tieude;
-    }
-
-    public function getMaBaiViet(){
-        return $this->ma_bviet;
+    public function getTitle(){
+        return $this->title;
     }
 }
--- CSE485_2023_BTTH02/services/ArticleService.php
+++ CSE485_2023_BTTH02/services/ArticleService.php
@@ -1,21 +1,21 @@
 <?php
 include("configs/DBConnection.php");
 include("models/Article.php");
-class ArticleService {
+class ArticleService{
     public function getAllArticles(){
         // 4 bước thực hiện
        $dbConn = new DBConnection();
        $conn = $dbConn->getConnection();
 
         // B2. Truy vấn
-        $sql = "SELECT * FROM baiviet";
+        $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id";
         $stmt = $conn->query($sql);
 
         // B3. Xử lý kết quả
         $articles = [];
         while($row = $stmt->fetch()){
-            $article = new Article($row['hinhanh'], $row['tieude'], $row['ma_bviet']);
-            array_push($articles, $article);
+            $article = new Article($row['title'], $row['summary'], $row['name']);
+            array_push($articles,$article);
         }
         // Mảng (danh sách) các đối tượng Article Model
 
